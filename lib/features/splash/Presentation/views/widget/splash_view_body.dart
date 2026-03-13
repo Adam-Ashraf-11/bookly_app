b66bd5c0f4267ddf65b68272d170c28df92358e8
Adam Ashraf <adamashrafhamdy11@gmail.com>
2026-03-13 07:04:18 +0200

    NavigatToHome

--- lib/features/splash/Presentation/views/widget/splash_view_body.dart
+++ lib/features/splash/Presentation/views/widget/splash_view_body.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bookly_app/core/utils/assets.dart';
+import 'package:flutter_bookly_app/features/home/presentation/view/home_view.dart';
+import 'package:get/get.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    navigatToHome();
+    super.initState();
+  }
+
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +31,23 @@ class SplashViewBody extends StatelessWidget {
           Text(
             "Read free Books",
             textAlign: TextAlign.center,
-            style: TextStyle(fontWeight: FontWeight.bold , fontSize: 22),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
           ),
         ],
+      ),
+    );
+  }
+
+
+//*  navigatToHome Method
+
+   void navigatToHome() {
+     Future.delayed(
+      Duration(seconds: 3),
+      () => Get.to(
+        () => HomeView(),
+        transition: Transition.fade,
+        duration: Duration(seconds: 2),
       ),
     );
   }
