7eddc176f73706127bbe4762d3a943dd4d2e7b2d
Adam Ashraf <adamashrafhamdy11@gmail.com>
2026-04-01 20:32:09 +0200

    add api servise

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_bookly_app/core/errors/failures.dart';
+import 'package:flutter_bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:flutter_bookly_app/features/home/data/repos/home_repo.dart';
+
+class HomeRepoImpl implements HomeRepo{
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
